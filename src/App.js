49236691f4498fb8b309e009d20e9be1feaa2aaf
Diwakar Naidu Dabbara <50259670+dewalker07@users.noreply.github.com>
2019-05-15 14:13:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Aboutus from './components/Aboutus';
-import Services from './components/Services';
-import Portfolio from './components/Portfolio';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
+// import Aboutus from './components/Aboutus';
+// import Services from './components/Services';
+// import Portfolio from './components/Portfolio';
+// import Blog from './components/Blog';
+// import Contact from './components/Contact';
 import {BrowserRouter, Route} from 'react-router-dom';
 // import './App.css';
 
@@ -17,12 +17,13 @@ export default class App extends Component {
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route path={process.env.PUBLIC_URL + '/home'} component={Home} />
-        <Route path={process.env.PUBLIC_URL + '/aboutus'} component={Aboutus} />
-        <Route path={process.env.PUBLIC_URL + '/services'} component={Services} />
-        <Route path={process.env.PUBLIC_URL + '/portfolio'} component={Portfolio} />
-        <Route path={process.env.PUBLIC_URL + '/blog'} component={Blog} />
-        <Route path={process.env.PUBLIC_URL + '/contact'} component={Contact} />
+    <Home />
+//         <Route path={process.env.PUBLIC_URL + '/home'} component={Home} />
+//         <Route path={process.env.PUBLIC_URL + '/aboutus'} component={Aboutus} />
+//         <Route path={process.env.PUBLIC_URL + '/services'} component={Services} />
+//         <Route path={process.env.PUBLIC_URL + '/portfolio'} component={Portfolio} />
+//         <Route path={process.env.PUBLIC_URL + '/blog'} component={Blog} />
+//         <Route path={process.env.PUBLIC_URL + '/contact'} component={Contact} />
       </div>
     </BrowserRouter>
   );
