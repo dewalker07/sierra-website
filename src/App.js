672aed28e28b815e1f96df7056c6c7f2f625a1de
Diwakar Naidu Dabbara <50259670+dewalker07@users.noreply.github.com>
2019-05-15 17:46:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {HashRouter, Route} from 'react-router-dom';
 // import './App.css';
 
 export default class App extends Component {
   render() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <Navbar />
         <Route path={`${process.env.PUBLIC_URL}/home`} component={Home} />
@@ -24,7 +24,7 @@ export default class App extends Component {
         <Route path={`${process.env.PUBLIC_URL}/blog`} component={Blog} />
         <Route path={`${process.env.PUBLIC_URL}/contact`} component={Contact} />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
   }
 }
