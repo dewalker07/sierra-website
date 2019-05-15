296c73fc7491515261bbedc3c08bced58cc6cd21
Diwakar <dabbara.divakar@gmail.com>
2019-05-15 13:41:26 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Aboutus from './components/Aboutus';
-import Services from './components/Services';
-import Portfolio from './components/Portfolio';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
+// import Aboutus from './components/Aboutus';
+// import Services from './components/Services';
+// import Portfolio from './components/Portfolio';
+// import Blog from './components/Blog';
+// import Contact from './components/Contact';
 import {BrowserRouter, Route} from 'react-router-dom';
 // import './App.css';
 
@@ -17,12 +17,13 @@ export default class App extends Component {
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route path={process.env.PUBLIC_URL + '/home'} component={Home} />
+        <Home />
+        {/* <Route path={process.env.PUBLIC_URL + '/home'} component={Home} />
         <Route path="/aboutus" component={Aboutus} />
         <Route path="/services" component={Services} />
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/blog" component={Blog} />
-        <Route path="/contact" component={Contact} />
+        <Route path="/contact" component={Contact} /> */}
       </div>
     </BrowserRouter>
   );
