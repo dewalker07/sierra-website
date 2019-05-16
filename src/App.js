ea0025f9a0b6a9745ab0ad3ec0a3e9ee144c92dc
Diwakar <dabbara.divakar@gmail.com>
2019-05-16 13:40:15 +0530

    changes done in app.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import './App.css';
 
 export default class App extends Component {
   render() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
+    <BrowserRouter basename={'/'}>
         <Navbar />
+        <Switch>
         <Route path='/home' component={Home} />
         <Route path='/aboutus' component={Aboutus} />
         <Route path='/services' component={Services} />
         <Route path='/portfolio' component={Portfolio} />
         <Route path='/blog' component={Blog} />
         <Route path='/contact' component={Contact} />
-      </div>
+        </Switch>
     </BrowserRouter>
   );
   }
